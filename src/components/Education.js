2fee52a02d58fdf46ac0cbea280b8dcd9c29eb10
YevaUsatova <yeva.usatova@gmail.com>
2021-08-31 14:17:26 -0400

    home

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Education (){
+    const [school, setSchool]= useState([])
+    useEffect(()=>{
+        fetch("http://localhost:3000/education")
+        .then((r)=> r.json())
+        .then((data)=> {
+             setSchool(data)       
+        })
+    },[])
+    const listOfSchools= school.map((list)=> <li key={list.id}>{list.name} 
+    {list.year} {list.faculty} {list.degree}</li>)
     return(
-    <> educ</>
+    <div> 
+        <ul>
+            {listOfSchools}
+        </ul>
+    </div>
     )
 }
 export default Education;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Home (){
-    
-    
-    
+    const email = "yeva.usatova@gmail.com"
     return( 
     <div> 
-        blah
+        <h1>Hi there</h1>
+        <h2>My name is Yeva Usatova</h2>
+        <h2>My contact email is {email} </h2>
+
     </div>
     )
 }
